4de4a37659eaffe577b1f1a8d49b4da914168235
u386361 <Ty_Thomas@bcbsil.com>
2018-10-01 14:48:37 -0500

    adding taxes

---
+++ etsy-info/src/main/java/com/runnersports/etsy/etsyinfo/controller/OrdersController.java
@@ -0,0 +1,41 @@
+package com.runnersports.etsy.etsyinfo.controller;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
+import java.math.BigDecimal;
+
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVRecord;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/order")
+public class OrdersController {
+
+	@GetMapping("/taxes")
+	public void getTaxes() throws IOException {
+
+		Iterable<CSVRecord> records = getRecords();
+		int totalTaxedOrders = 0;
+		BigDecimal totalTaxes = new BigDecimal("0");
+		BigDecimal totalOrderValue = new BigDecimal("0");
+		for (CSVRecord record : records) {
+			String state = record.get("Ship State");
+			if ("IL".equals(state)) {
+				totalTaxedOrders++;
+				totalTaxes = totalTaxes.add(new BigDecimal(record.get("Sales Tax")));
+				totalOrderValue = totalOrderValue.add(new BigDecimal(record.get("Order Value")));
+			}
+		}
+		
+		System.out.println(totalTaxedOrders + " taxed orders, totalling: " + totalOrderValue + " and withheld " + totalTaxes);
+	}
+
+	private Iterable<CSVRecord> getRecords() throws IOException {
+		Reader in = new FileReader("c:\\Users\\u386361\\Downloads\\EtsySoldOrders2018.csv");
+		return CSVFormat.RFC4180.withHeader().parse(in);
+	}
+}
